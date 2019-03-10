8220502f90b9f6acf60e068a78de892971030dfb
ninedraft <clamores.pro@gmail.com>
2019-03-10 13:48:07 +0300

    add ParseZIPArchive func prototype

--- pkg/navigator/parser.go
+++ pkg/navigator/parser.go
@@ -1,1 +1,16 @@
 package navigator
+
+import (
+	"archive/zip"
+
+	"github.com/pkg/errors"
+)
+
+func ParseZIPArchive(filePath string) (*Root, error) {
+	var reader, errOpenZIP = zip.OpenReader(filePath)
+	if errOpenZIP != nil {
+		return nil, errors.Wrap(errOpenZIP, "unable to open notion backup file")
+	}
+	defer reader.Close()
+	return nil, nil
+}
